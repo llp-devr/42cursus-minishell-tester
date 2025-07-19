9f80388f44ef06a7a46e35013a0ce501091e6bd4
Leonardo Lopes Pereira <lepereir@student.42.fr>
2025-07-19 13:55:33 +0200

    update

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import colorama
 import os
 import pexpect
 import re
+import tempfile
 import yaml
 
 from colorama import Fore, Style
 
 colorama.init(autoreset=True)
-
 
 class ShellPty:
     def __init__(self, cmd, args, prompt):
@@ -16,6 +16,10 @@ class ShellPty:
         env = {"PATH": "/bin:/usr/bin"}
         env["USER"] = os.environ.get("USER", "unknown")
         env["HOME"] = os.environ.get("HOME", "/tmp")
+        env["SHELL"] = "/bin/crashinette"
+        tmp = tempfile.NamedTemporaryFile(delete=False)
+        env["TMPFILE"] = tmp.name
+        self.tempfile = tmp.name
         self.child = pexpect.spawn(cmd, args=args, encoding="utf-8", env=env)
         self.child.setwinsize(30, 60)
         self.child.expect(self.prompt)
@@ -27,8 +31,8 @@ class ShellPty:
         # COMMAND
         self.child.sendline(command)
         self.child.expect("\n")
-        if len(command) > 57:
-            output += f"crashinette>> {command[:43]}...\n"
+        if len(command) > 40:
+            output += f"crashinette>> {command[:40]}...\n"
         else:
             output += f"crashinette>> {command}\n"
 
@@ -39,18 +43,21 @@ class ShellPty:
         # EXIT CODE
         self.child.sendline("echo $?")
         self.child.expect(self.prompt)
-        output += f">> {self.child.before}"
+        output += f"crashinette>> {self.child.before}"
 
         return output
 
     def close(self):
+        output = "\n\n"
         self.child.sendline("exit 0")
         self.child.expect(pexpect.EOF)
+        output += "EXIT REPORT:\n"
+        output += f"{self.child.before}"
+        output += "\n\n"
+        output += f"Exit status: {self.child.exitstatus}"
+        return output
 
-        print(f"Exit status: {self.child.exitstatus}")
-
-
-def assert_equals(output1, output2):
+def assert_equals(output1, output2, tmpfile1, tmpfile2):
     lines1 = output1.strip().splitlines()
     lines2 = output2.strip().splitlines()
 
@@ -66,9 +73,8 @@ def assert_equals(output1, output2):
     for l1, l2 in zip(lines1, lines2):
         if l1 == l2:
             color = Fore.GREEN
-        elif l1.replace("minishell", "crashinette").replace(
-            "bash", "crashinette"
-        ) == l2.replace("minishell", "crashinette").replace("bash", "crashinette"):
+        elif l1.replace("minishell", "crashinette").replace("bash", "crashinette").replace(tmpfile1, "/tmp/crashinette").replace(tmpfile2, "/tmp/crashinette"
+        ) == l2.replace("minishell", "crashinette").replace("bash", "crashinette").replace(tmpfile1, "/tmp/crashinette").replace(tmpfile2, "/tmp/crashinette"):
             color = Fore.MAGENTA
         else:
             color = Fore.RED
@@ -80,7 +86,7 @@ def assert_equals(output1, output2):
 
 def banner():
     print("")
-    print(f"{Fore.GREEN}|{"Minishell":<60}|{"Bash":<60}|")
+    print(f"{Fore.GREEN}|{'Minishell':<60}|{'Bash':<60}|")
 
 
 def main():
@@ -113,7 +119,21 @@ def main():
     print(f"Bash command: {args.bash}")
 
     bash = ShellPty(cmd=args.bash, args=["--posix"], prompt=args.bash_prompt)
-    minishell = ShellPty(cmd=args.minishell, args=[], prompt=args.minishell_prompt)
+    minishell = ShellPty(
+    cmd="valgrind", 
+    args=[
+        "--quiet",
+        "--tool=memcheck", 
+        "--track-fds=yes",
+        "--leak-check=full",
+        "--show-leak-kinds=all",
+        "--suppressions=minishell.suppress",
+        "--error-exitcode=1",
+        "--exit-on-first-error=yes",
+        args.minishell
+    ],
+    prompt=args.minishell_prompt
+)
 
     with open(args.tests, "r") as file:
         tests = yaml.safe_load(file)
@@ -121,12 +141,10 @@ def main():
         banner()
 
         for command in tests["commands"]:
-            if not assert_equals(minishell.exec(command), bash.exec(command)):
+            if not assert_equals(minishell.exec(command), bash.exec(command), minishell.tempfile, bash.tempfile):
                 break
-
-    bash.close()
-    minishell.close()
-
+    
+    assert_equals(minishell.close(), bash.close(), minishell.tempfile, bash.tempfile)
 
 if __name__ == "__main__":
     main()
